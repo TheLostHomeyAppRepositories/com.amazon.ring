6d625589daf1a7be351284fc2f0effd499ecb6cf
Danee de Kruijff <daneedekruyff@outlook.com>
2024-12-17 20:23:24 +0100

    Ring Contact Sensor

---
+++ drivers/contactsensor/driver.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const Homey = require('homey');
+const Driver = require('../../lib/Driver.js');
+
+class DriverContactSensor extends Driver {
+
+    onInit() {
+        this.log('onInit');
+    }
+
+    onPairListDevices(data, callback) {
+        this.log('onPairListDevicesContactSensor');
+
+        return new Promise((resolve, reject) => {
+            let foundDevices = [];
+
+            this.homey.app.getRingAlarmDevices((error, result) => {
+                if (error) {
+                    return this.error(error);
+                }
+
+                result.forEach(async (device) => {
+                    if (device.data.deviceType === 'sensor.contact') {
+                        foundDevices.push({
+                            name : device.data.name,
+                            data : {
+                                id: device.data.serialNumber,
+                                catalogId: device.data.catalogId
+                            }
+                        });    
+
+                    }
+                })
+
+                resolve(foundDevices);
+            });
+        });
+    }
+}
+
+module.exports = DriverContactSensor;
